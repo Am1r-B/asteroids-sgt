ebd7482831884e9f927c6ad0c87cae894ecfc3ad
Am1r-B_PC <69463011+Am1r-B@users.noreply.github.com>
2022-08-20 20:05:31 +0430

    drawing partially off-screen objects

--- asteroids/main.lua
+++ asteroids/main.lua
@@ -33,19 +33,27 @@ function love.update(dt)
 end
 
 function love.draw()
-  love.graphics.setColor(0, 0, 1)
-  love.graphics.circle('fill', shipX, shipY, 30)
-  
-  local shipCircleDistance = 20
-  love.graphics.setColor(0, 1, 1)
-  love.graphics.circle(
-    'fill',
-    shipX + math.cos(shipAngle) * shipCircleDistance,
-    shipY + math.sin(shipAngle) * shipCircleDistance,
-    5
-  )
+  for y = -1, 1 do
+    for x = -1, 1 do
+      love.graphics.origin()
+      love.graphics.translate(x * arenaWidth, y * arenaHeight)
+      
+      love.graphics.setColor(0, 0, 1)
+      love.graphics.circle('fill', shipX, shipY, 30)
+      
+      local shipCircleDistance = 20
+      love.graphics.setColor(0, 1, 1)
+      love.graphics.circle(
+        'fill',
+        shipX + math.cos(shipAngle) * shipCircleDistance,
+        shipY + math.sin(shipAngle) * shipCircleDistance,
+        5
+      )
+    end
+  end
   
   -- Temporary
+  love.graphics.origin()
   love.graphics.setColor(1, 1, 1)
   love.graphics.print(table.concat({
     'shipAngle: '..shipAngle,
